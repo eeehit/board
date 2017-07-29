5295f72745dc2a0739e885719660c7ddcc1d363a
eeehit <eeehit.sytem@gmail.com>
2017-07-29 22:43:55 +0900

    main Page

--- src/main/java/com/eeehit/board/HelloController.java
+++ src/main/java/com/eeehit/board/HelloController.java
@@ -2,11 +2,15 @@ package com.eeehit.board;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class HelloController {
 	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("main");
+		return mv;
 	}
 }

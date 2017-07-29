cca5d1f8d5f782c86c8f11e7839a4a22b70c1367
eeehit <eeehit.sytem@gmail.com>
2017-07-29 19:57:34 +0900

    first commit

---
+++ src/main/java/com/eeehit/board/BoardApplication.java
@@ -0,0 +1,22 @@
+package com.eeehit.board;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+		};
+	}
+}
